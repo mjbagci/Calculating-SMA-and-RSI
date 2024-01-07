d28c704a85936bdde8a146fabdc54031d464905a
Muhammet İsa <139056518+mjbagci@users.noreply.github.com>
2024-01-07 14:42:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 
+from setuptools import setup, find_packages
+
+setup(
+    name='Calculating sma and rsi’,
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    "tqdm",
+    "six",
+    "requests",
+],
+entry_points={
+    'console_scripts': [
+        'calculate_rsi = your_package_name.main:calculate_rsi_function',
+    ],
+},
+    author=Muhammet Isa Bagci',
+    description='Description of your package',
+    url="https://github.com/mjbagci/Calculating-SMA-and-RSI/edit/main/setup.py"
+)
